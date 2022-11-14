089675cd83df02250b295150615c07f3c51f3c31
kkl4846 <kkl4846@cau.ac.kr>
2022-11-15 01:55:35 +0900

    Feat/#1: 도서 조회 응답 메세지 추가

--- src/module/responseMessage.ts
+++ src/module/responseMessage.ts
@@ -4,6 +4,9 @@ const message = {
     BAD_REQUEST: '잘못된 요청',
     INTERNAL_SERVER_ERROR: '서버 내부 오류',
 
+    // 도서 관련
+    GET_BOOK_SUCCESS: '도서 조회 성공',
+    NOT_FOUND_BOOK: '해당 책이 존재하지 않습니다',
 }
 
 export default message;
