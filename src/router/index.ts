f2e9d741209e9319c4e667c79183592c165ecaf6
YuSuhwa-ve <dhfhfkzjxms@sookmyung.ac.kr>
2022-11-15 14:21:07 +0900

    Feat/#2 : 최초커밋'

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import userRouter from "./userRouter";
+import bookRouter from "./bookRouter";
 
 const router: Router = Router();
 
 router.use("/user", userRouter);
+
+router.use("/book",bookRouter);
 
 export default router;
